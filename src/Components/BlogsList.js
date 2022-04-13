66cd1b9a0be8df3a6d55933f3848076d24da2ddd
Kwame007 <clementnyankuadjei@gmail.com>
2022-04-13 12:19:30 +0000

    set list keys

--- src/Components/BlogsList.js
+++ src/Components/BlogsList.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Blogs from "./Blogs";
 import Styles from "./BlogList.module.css";
 import useFetchData from "./useFetchData";
 
 const BlogList = () => {
-  useFetchData("http://localhost:8000/blog");
+  const { data, isLoading, error } = useFetchData(
+    "http://localhost:8000/blogs"
+  );
+  console.log(data);
 
   return (
     <div className={Styles.content}>
       <h2>All Blogs</h2>
-      <Blogs />
-      <Blogs />
-      <Blogs />
-      <Blogs />
+      {/* Dynamic content rendering */}
+      {isLoading && <p>Loading</p>}
+      {!isLoading && data.map((blog) => <Blogs blog={blog} key={blog.id} />)}
     </div>
   );
 };
