fd31b2afbce6e4874ffe7f8036cdb605a09025ed
Kwame007 <clementnyankuadjei@gmail.com>
2022-04-13 12:11:52 +0000

    add debounce to data fatch

--- src/Components/useFetchData.js
+++ src/Components/useFetchData.js
@@ -6,28 +6,41 @@ const useFetchData = (url) => {
   // states to manage [data,loading,error]
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(null);
 
   // handle side effect {using side effect to handle fetch()}
   useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          // setIsLoading(true);
-          throw new Error("Could not fetch data");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setError(true);
-        setIsLoading(false);
-        console.log(err.message);
-      });
+    const abortFetch = new AbortController();
+    setTimeout(() => {
+      fetch(url, { signal: abortFetch.signal })
+        .then((res) => {
+          if (!res.ok) {
+            // setIsLoading(true);
+            throw new Error("Could not fetch data");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setError(true);
+            setIsLoading(false);
+            console.log(err.message);
+          }
+        });
+    }, 1000);
+    // useEffect clean function
+    return () => {
+      // abort data fetch before next request
+      abortFetch.abort();
+    };
   }, [url]);
-  console.log(data, isLoading, error);
+  return { data, isLoading, error };
 };
 export default useFetchData;
