73f5267d8937d27916fb79b43eb4fee2cc1546c6
Kwame007 <clementnyankuadjei@gmail.com>
2022-04-14 23:56:12 +0000

    add blog details component

---
+++ src/Components/BlogDetails.js
@@ -0,0 +1,50 @@
+import useFetchData from "./useFetchData";
+import { useParams, useNavigate } from "react-router-dom";
+
+import Styles from "./BlogDetails.module.css";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const { data, isLoading } = useFetchData(
+    `http://localhost:8000/blogs/${id} `
+  );
+  const navigate = useNavigate();
+
+  // handle delete function
+  const handleDelete = () => {
+    fetch(`http://localhost:8000/blogs/${id} `, { method: "DELETE" })
+      .then(() =>
+        // redirect to home after delete {(-1) return to prev page before deletion}
+        navigate(-1)
+      )
+      .catch((err) => console.log(err.message));
+  };
+  // handle delete function
+  const handleEdit = () => {
+    console.log("edit clicked");
+  };
+  return (
+    <>
+      <div
+        className={Styles["blog-details"]}
+        style={{ maxWidth: "700px", margin: "100px auto" }}
+      >
+        {isLoading && <h4 className="loading">Loading...</h4>}
+        {!isLoading && (
+          <article>
+            <h2>{data.title}</h2>
+            <h5>
+              Written by : <span className={Styles.author}>{data.author}</span>{" "}
+            </h5>
+            <div>{data.body}</div>
+            <div className={Styles["btn-group"]}>
+              <button onClick={handleEdit}>Edit</button>
+              <button onClick={handleDelete}>Delete blog</button>
+            </div>
+          </article>
+        )}
+      </div>
+    </>
+  );
+};
+export default BlogDetails;
